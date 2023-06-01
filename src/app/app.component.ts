efacc40327390c9ef068801eb03c783cea375dd8
yafit <yafit.hamilton@cellebrite.com>
2023-06-01 15:10:57 +0300

    adding files to Github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pw';
+  length = 0;
+  password = '';
+  isuseNumbers = false;
+  isuseLetters = false;
+  isuseSymbols = false;
+
+  OnChangeUseNumbers() {
+    this.isuseNumbers = !this.isuseNumbers;
+  }
+  OnChangeUseLetters() {
+    this.isuseLetters = !this.isuseLetters;
+  }
+  OnChangeUseSymbols() {
+    this.isuseSymbols = !this.isuseSymbols;
+  }
+  OnButtonClick() {
+    this.password = "'fgfg";
+  }
+
+  onChangeLenght(event: any) {
+    const inputValue = event.target.value;
+    var res = parseInt(inputValue);
+    if (!isNaN(res)) {
+      this.length = res;
+    }
+  }
 }
